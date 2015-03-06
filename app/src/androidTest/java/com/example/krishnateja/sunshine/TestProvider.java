9211fe1fb65592fc30e39e905e3f2c18b33e0a95
krishna teja velagapudi <ktv205@nyu.edu>
2015-03-05 23:40:17 -0500

    lesson 4a

---
+++ app/src/androidTest/java/com/example/krishnateja/sunshine/TestProvider.java
@@ -0,0 +1,7 @@
+package com.example.krishnateja.sunshine;
+
+/**
+ * Created by krishnateja on 3/5/2015.
+ */
+public class TestProvider {
+}
--- app/src/main/java/com/example/krishnateja/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/krishnateja/sunshine/DetailActivity.java
@@ -1,11 +1,15 @@
 package com.example.krishnateja.sunshine;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.annotation.Nullable;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+
+import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -17,6 +21,8 @@ import android.widget.TextView;
 
 
 public class DetailActivity extends ActionBarActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +41,15 @@ public class DetailActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_detail, menu);
+
+        // Return true to display menu
         return true;
+
     }
+
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -58,7 +69,10 @@ public class DetailActivity extends ActionBarActivity {
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
+        ShareActionProvider mShareActionProvider;
         View rootView;
+        private String mForecast;
+        private final static String TAG="PlaceholderFragment";
 
         public PlaceholderFragment() {
         }
@@ -66,6 +80,32 @@ public class DetailActivity extends ActionBarActivity {
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
+            setHasOptionsMenu(true);
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            inflater.inflate(R.menu.detailfragment,menu);
+            // Locate MenuItem with ShareActionProvider
+            MenuItem item = menu.findItem(R.id.menu_item_share);
+
+            // Fetch and store ShareActionProvider
+            mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+            if (mShareActionProvider != null) {
+                mShareActionProvider.setShareIntent(createShareIntent());
+            }
+        }
+        private Intent createShareIntent(){
+            Intent shareIntent=new Intent(Intent.ACTION_SEND);
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT,mForecast);
+            return shareIntent;
+
+        }
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            return super.onOptionsItemSelected(item);
         }
 
         @Override
@@ -79,9 +119,9 @@ public class DetailActivity extends ActionBarActivity {
         public void onActivityCreated(@Nullable Bundle savedInstanceState) {
             super.onActivityCreated(savedInstanceState);
             Bundle bundle=getArguments();
-            String forecast=bundle.get(Intent.EXTRA_TEXT).toString();
+            mForecast=bundle.get(Intent.EXTRA_TEXT).toString();
             TextView textView=(TextView)rootView.findViewById(R.id.fragment_detail_textview);
-            textView.setText(forecast);
+            textView.setText(mForecast);
         }
     }
 }
